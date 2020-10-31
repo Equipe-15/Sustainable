58fe182654e7ac7a7a54f011f740fe437ac4d2f9
Ilgsson Braga <ilgssonbraga@pop-os.localdomain>
2020-10-31 14:53:35 -0300

    feat: add login

--- src/js/controllers/LoginController.js
+++ src/js/controllers/LoginController.js
@@ -4,15 +4,27 @@ function login() {
   const getSenha = document.querySelector("#password");
   console.log(userInfo);
 
-  if (userInfo.email === getEmail.value && userInfo.senha === getSenha.value) {
-    localStorage.setItem(
-      "SustainableLogin",
-      JSON.stringify({ isLogged: true })
-    );
-    window.location = "meuPerfil.html";
-  } else {
+  if (!userInfo) {
     alert("Usuário não encontrado!");
+  } else {
+    if (
+      
+      userInfo.email === getEmail.value &&
+
+           userInfo.senha === getSenha.value
+    
+    ) {
+      localStorage.setItem(
+        "SustainableLogin",
+        JSON.stringify({ isLogged: true })
+      );
+      window.location = "meuPerfil.html";
+    } else {
+      alert("Usuário não encontrado!");
+    }
   }
+
+ 
 
   getEmail.value = "";
   getSenha.value = "";
