80efeb36d51362c3035548bc05429f11e68ea737
Ilgsson Braga <ilgssonbraga@localhost.localdomain>
2020-11-14 10:50:13 -0300

    feat: cadastro material

---
+++ src/js/controllers/CadastroMaterialController.js
@@ -0,0 +1,26 @@
+function storeMaterial() {
+  const getTipo = document.querySelector("#tipo");
+  const getLocal = document.querySelector("#local");
+  const getData = document.querySelector("#data");
+
+  const materiais = JSON.parse(localStorage.getItem("Materiais"));
+
+  const data = {
+    tipo: getTipo.value,
+    local: getLocal.value,
+    data: getData.value,
+  };
+
+  if (materiais) {
+    const saveMateriais = [...materiais, data];
+    localStorage.setItem("Materiais", JSON.stringify(saveMateriais));
+  } else {
+    localStorage.setItem("Materiais", JSON.stringify([data]));
+  }
+
+  getTipo.value = "";
+  getLocal.value = "";
+  getData.value = "";
+
+  window.location = "meuPerfil.html";
+}
